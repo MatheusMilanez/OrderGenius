439bd6e0265be5aa2b2e396ece13370628b2e81c
Matheus Milanez <mmilanez@APLICACAO1.BR>
2024-02-26 18:00:30 -0300

    Update

--- app.js
+++ app.js
@@ -41,6 +41,24 @@ app.get('/table', (req, res) => {
     res.sendFile(__dirname + '/table.html');
 });
 
+app.get('/kitchen', (req, res) => {
+    res.sendFile(__dirname + '/kitchen.html');
+});
+
+app.get('/qrcode', (req, res) => {
+    res.sendFile(__dirname + '/qrcode.html');
+});
+
+app.get('/requests', (req, res) => {
+    res.sendFile(__dirname + '/requests.html');
+});
+
+//Routes users
+
+app.get('/user_requests', (req, res) => {
+    res.sendFile(__dirname + '/user_requests.html');
+});
+
 
 //Login Form Router 
 
@@ -72,7 +90,7 @@ app.post('/login',(req,res)=>{
 
 // Register or update Product
 app.post('/product', (req, res) => {
-    const { name, price, expiration_date, quantity } = req.body;
+    const { name, price, expiration_date, quantity, category } = req.body;
 
     // Checks to see if the product already exists in the database
     db.get('SELECT * FROM product WHERE name = ?', [name], (err, row) => {
@@ -94,7 +112,7 @@ app.post('/product', (req, res) => {
             });
         } else {
             // If the product does not exist, enter a new record
-            db.run('INSERT INTO product (name, price, expiration_date, quantity) VALUES (?, ?, ?, ?)', [name, price, expiration_date, quantity], (err) => {
+            db.run('INSERT INTO product (name, price, expiration_date, quantity, category) VALUES (?, ?, ?, ?, ?)', [name, price, expiration_date, quantity, category], (err) => {
                 if (err) {
                     console.error('Erro ao cadastrar o produto:', err);
                     return res.status(500).send('Erro ao cadastrar o produto');
@@ -142,7 +160,7 @@ app.get('/products',(req,res)=>{
 
 // Editar produto
 app.post('/product/edit', (req, res) => {
-    const { id, name, price, expiration_date, quantity } = req.body;
+    const { id, name, price, expiration_date, quantity, category } = req.body;
 
     db.get('SELECT * FROM product WHERE id = ?', [id], (err, row) => {
         if (err) {
@@ -152,7 +170,7 @@ app.post('/product/edit', (req, res) => {
 
         if (row) {
             // Se o produto existir, atualize os dados
-            db.run('UPDATE product SET name = ?, price = ?, expiration_date = ?, quantity = ? WHERE id = ?', [name, price, expiration_date, quantity, id], (err) => {
+            db.run('UPDATE product SET name = ?, price = ?, expiration_date = ?, quantity = ?, category = ? WHERE id = ?', [name, price, expiration_date, quantity, category, id], (err) => {
                 if (err) {
                     console.error('Erro ao atualizar o produto:', err);
                     return res.status(500).send('Erro ao atualizar o produto');
@@ -169,44 +187,158 @@ app.post('/product/edit', (req, res) => {
 });
 
 
-
-//Tables
-
-app.post('/table', (req, res) => {
-    const { name_table } = req.body;
-
-    console.log(`Nome recebido:${name_table}`);
-    // Geração do QR Code
-    const qrCodeData = `Nome da Mesa: ${name_table}`;
-    qr.toDataURL(qrCodeData, (err, qrCodeURL) => {
+// Endpoint para carregar produtos
+app.get('/products', (req, res) => {
+    db.all('SELECT * FROM product', (err, rows) => {
         if (err) {
-            console.error('Erro ao gerar o QR Code:', err);
-            return res.status(500).send('Erro ao gerar o QR Code');
-        }
-
-        // Inserção da mesa no banco de dados
-        db.run('INSERT INTO tables_ (name_table, qrcode) VALUES (?, ?)', [name_table, qrCodeURL], (err) => {
-            if (err) {
-                console.error('Erro ao cadastrar a mesa:', err);
-                return res.status(500).send('Erro ao cadastrar a mesa');
-            }
-            console.log('Mesa cadastrada com sucesso');
-            res.redirect('/table');
-        });
-    });
-});
-
-// List tables
-
-app.get('/tables', (req, res) => {
-    db.all('SELECT * FROM tables_', (err, rows) => {
-        if (err) {
-            console.error('Erro ao obter a lista de mesas:', err);
-            return res.status(500).send('Erro ao obter a lista de mesas');
+            console.log('Erro ao obter a lista de produtos', err);
+            return res.status(500).send('Erro ao obter a lista de produtos');
         }
         res.json(rows);
     });
 });
+
+// Estrutura de dados para armazenar temporariamente os itens do carrinho
+let cartItems = [];
+
+// Endpoint para adicionar produto ao carrinho com quantidade
+app.post('/add-to-cart', (req, res) => {
+    const { productId, quantity } = req.body;
+
+    db.get('SELECT * FROM product WHERE id = ?', [productId], (err, row) => {
+        if (err) {
+            console.error('Erro ao buscar o produto:', err);
+            return res.status(500).send('Erro ao buscar o produto');
+        }
+
+        if (row) {
+            const product = {
+                id: row.id,
+                name: row.name,
+                price: row.price,
+                quantity: quantity
+            };
+            cartItems.push(product);
+            res.sendStatus(200);
+        } else {
+            res.status(404).send('Produto não encontrado');
+        }
+    });
+});
+
+// Endpoint para confirmar o pedido
+app.post('/confirm-order', async (req, res) => {
+    const { tableNumber, userName } = req.body;
+    const totalBalance = cartItems.reduce((total, item) => total + (item.price * item.quantity), 0);
+
+    // Verifica se o carrinho não está vazio
+    if (cartItems.length === 0) {
+        return res.status(400).send('O carrinho está vazio. Adicione produtos antes de confirmar o pedido.');
+    }
+
+    try {
+        // Extrai apenas os nomes dos produtos
+        const productNames = cartItems.map(item => item.name);
+
+        // Inicia uma transação
+        await runTransaction(async (db) => {
+            // Busca os IDs dos produtos no banco de dados
+            const productIds = cartItems.map(item => item.id);
+
+            // Busca os nomes dos produtos correspondentes aos IDs
+            const productNames = await Promise.all(productIds.map(async (id) => {
+                return new Promise((resolve, reject) => {
+                    db.get('SELECT name FROM product WHERE id = ?', [id], (err, row) => {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve(row.name);
+                        }
+                    });
+                });
+            }));
+
+            // Insere o pedido no banco de dados com os nomes dos produtos
+            await db.run('INSERT INTO requests (product, number_table, user_name, total_balance) VALUES (?, ?, ?, ?)', [JSON.stringify(productNames), tableNumber, userName, totalBalance]);
+            cartItems = []; // Limpa o carrinho após o pedido ser confirmado
+            res.sendStatus(200);
+        });
+    } catch (error) {
+        console.error('Erro ao confirmar o pedido:', error);
+        return res.status(500).send('Erro ao confirmar o pedido');
+    }
+});
+
+
+// Função para executar uma transação
+function runTransaction(callback) {
+    return new Promise((resolve, reject) => {
+        db.serialize(() => {
+            db.run('BEGIN TRANSACTION');
+            callback(db)
+                .then(() => {
+                    db.run('COMMIT', (err) => {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve();
+                        }
+                    });
+                })
+                .catch((error) => {
+                    db.run('ROLLBACK');
+                    reject(error);
+                });
+        });
+    });
+}
+
+
+// Endpoint para obter os pedidos
+app.get('/orders', (req, res) => {
+    db.all('SELECT * FROM requests', (err, rows) => {
+        if (err) {
+            console.log('Erro ao obter os pedidos', err);
+            return res.status(500).send('Erro ao obter os pedidos');
+        }
+
+        // Para cada pedido, buscar os detalhes dos produtos associados
+        Promise.all(rows.map(row => {
+            return new Promise((resolve, reject) => {
+                db.all('SELECT * FROM product WHERE id IN (SELECT product_id FROM request_products WHERE request_id = ?)', [row.id], (err, products) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        row.products = products;
+                        resolve(row);
+                    }
+                });
+            });
+        })).then(ordersWithProducts => {
+            res.json(ordersWithProducts);
+        }).catch(error => {
+            console.error('Erro ao obter os detalhes dos produtos:', error);
+            return res.status(500).send('Erro ao obter os detalhes dos produtos');
+        });
+    });
+});
+
+// Endpoint para marcar um pedido como entregue
+app.post('/mark-as-delivered', (req, res) => {
+    const { orderId } = req.body;
+
+    db.run('UPDATE requests SET delivered = 1 WHERE id = ?', [orderId], (err) => {
+        if (err) {
+            console.error('Erro ao marcar o pedido como entregue:', err);
+            return res.status(500).send('Erro ao marcar o pedido como entregue');
+        }
+        res.sendStatus(200);
+    });
+});
+
+
+
+
 
 //Start server
 app.listen(PORT,()=>{
